35b6fc10063463d8c72c0647e115b2dd24715b2f
sumanthsprabhu <sumanths1988@gmail.com>
2025-01-28 13:57:21 +0530

    update yices URL

--- Manifest/yices.defs.sh
+++ Manifest/yices.defs.sh
@@ -5,26 +5,24 @@ pkg_name="yices-2.6.4"
 
 # Origin for binary distribution
 function pkg_bin_origin() {
-    #case "$CIAO_OS" in
-    #    LINUX)
-    #        pkg_tarfile="$pkg_name""-x86_64-unknown-linux-gnu-static-gmp.tar.gz" ;;
-    #    DARWIN)
-    #        pkg_tarfile="$pkg_name""-x86_64-apple-darwin15.2.0-static-gmp.tar.gz" ;;
-    #    *)
-    #        echo "ERROR: Unsupported CIAO_OS=$CIAO_OS" 1>&2
-    #       exit 1
-    #esac
+    case "$CIAO_OS" in
+        LINUX)
+            pkg_tarfile="$pkg_name""-x86_64-unknown-linux-gnu-static-gmp.tar.gz" ;;
+        DARWIN)
+            pkg_tarfile="$pkg_name""-x86_64-apple-darwin15.2.0-static-gmp.tar.gz" ;;
+        *)
+            echo "ERROR: Unsupported CIAO_OS=$CIAO_OS" 1>&2
+           exit 1
+    esac
     #pkg_url="http://yices.csl.sri.com/cgi-bin/yices2-newnewdownload.cgi?file=$pkg_tarfile&accept=I+Agree"
-    #pkg_url="https://yices.csl.sri.com/releases/2.6.5/yices-2.6.5-x86_64-pc-linux-gnu-static-gmp.tar.gz"
-    pkg_url=""
+    pkg_url="https://yices.csl.sri.com/releases/2.6.5/yices-2.6.5-x86_64-pc-linux-gnu-static-gmp.tar.gz"
 }
 
 # Origin for source distribution
 function pkg_src_origin() {
-    #pkg_tarfile="yices-2.6.5-src.tar.gz"
+    pkg_tarfile="yices-2.6.5-src.tar.gz"
     #pkg_url="http://yices.csl.sri.com/cgi-bin/yices2-newnewdownload.cgi?file=$pkg_tarfile&accept=I+Agree"
-    #pkg_url="https://yices.csl.sri.com/releases/2.6.5/yices-2.6.5-src.tar.gz"
-    pkg_url=""
+    pkg_url="https://yices.csl.sri.com/releases/2.6.5/yices-2.6.5-src.tar.gz"
 }
 
 # Fixes for binary distribution
@@ -34,17 +32,14 @@ function pkg_fix_bin() {
 
 # Build from source
 function pkg_build() {
-    #LDFLAGS="-L$THIRDPARTY/lib" CPPFLAGS="-I$THIRDPARTY/include" LD_LIBRARY_PATH="$THIRDPARTY/lib" ./configure
-    #make
-    true
+    LDFLAGS="-L$THIRDPARTY/lib" CPPFLAGS="-I$THIRDPARTY/include" LD_LIBRARY_PATH="$THIRDPARTY/lib" ./configure
+    make
 }
 
 # Install from source
 function pkg_install() {
     # NOTE: We do not use install-yices (our fix_dylib fixes dylib paths)
-    #cp -R "$srcdir/$pkg_name/build/"*"/dist/"* "$storedir/$pkg_name/"
-    echo "STORE DIR is: $storedir"
-    cp -R "$YICES_PKG_DIR/build/"*"/dist/"* "$storedir/$pkg_name/"
+    cp -R "$srcdir/$pkg_name/build/"*"/dist/"* "$storedir/$pkg_name/"
 }
 
 # Dynamic library name and files
